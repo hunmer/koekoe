683b003864cb229cada09a9a1d6fc853e035a9ea
樱川流夏立 <1312552724@qq.com>
2021-09-24 08:59:10 +0800

    Initial commit

---
+++ js/until.js
@@ -0,0 +1,46 @@
+String.prototype.replaceAll = function(s1, s2) {
+    return this.replace(new RegExp(s1, "gm"), s2);
+}
+
+var _GET = getGETArray();
+var g_localKey = 'mosya_';
+// 本地储存前缀
+var g_config = local_readJson('config', {
+
+});
+var g_data = local_readJson('data',);
+function local_saveJson(key, data) {
+    if (window.localStorage) {
+        key = g_localKey + key;
+        data = JSON.stringify(data);
+        if (data == undefined) data = '[]';
+        return localStorage.setItem(key, data);
+    }
+    return false;
+}
+
+function local_readJson(key, defaul) {
+    if (!window.localStorage) return defaul;
+    key = g_localKey + key;
+    var r = JSON.parse(localStorage.getItem(key));
+    return r === null ? defaul : r;
+}
+
+
+function getGETArray() {
+    var a_result = [],
+        a_exp;
+    var a_params = window.location.search.slice(1).split('&');
+    for (var k in a_params) {
+        a_exp = a_params[k].split('=');
+        if (a_exp.length > 1) {
+            a_result[a_exp[0]] = decodeURIComponent(a_exp[1]);
+        }
+    }
+    return a_result;
+}
+
+
+function randNum(min, max) {
+    return parseInt(Math.random() * (max - min + 1) + min, 10);
+}
