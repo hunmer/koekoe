741c253e7e6bd4e14a6e28f09d8cc40038044a6c
樱川流夏立 <1312552724@qq.com>
2021-09-24 09:07:16 +0800

    1

--- js/index.js
+++ js/index.js
@@ -1,37 +1,40 @@
 var g_keys;
 var g_audio;
 var g_id;
-$(function(){
+$(function() {
 
-	g_audio = $('audio')[0];
-	g_audio.onended = () => {
-		randomAudio();
-	}
+    g_audio = $('audio')[0];
+    g_audio.onended = () => {
+        randomAudio();
+    }
 
-	if(!g_data){
-		$.getJSON('./data.json', function(json, textStatus) {
-			if(textStatus == 'success'){
-				g_data = json;
-				local_saveJson('data', g_data);
-				initData();
-			}
-		});
-	}else{
-		initData();
-	}
+    if (!g_data) {
+        $.getJSON('./data.json', function(json, textStatus) {
+            if (textStatus == 'success') {
+                g_data = json;
+                local_saveJson('data', g_data);
+                initData();
+            }
+        });
+    } else {
+        initData();
+    }
 })
 
-function initData(){
-	g_keys = Object.keys(g_data);
-	randomAudio();
+function initData() {
+    g_keys = Object.keys(g_data);
+    randomAudio();
 }
 
-function randomAudio(){
-	var id = g_keys[randNum(1, g_keys.length)];
-	g_id = id;
-	var data = g_data[id];
-	document.title = data.title;
-	$('h3').html(data.title);
-	$('h5').html(data.user);
-	g_audio.src = 'https://file.koe-koe.com/sound/upload/'+id+'.mp3';
+function randomAudio() {
+    loadId(g_keys[randNum(1, g_keys.length)]);
+}
+
+function loadId(id) {
+    g_id = id;
+    var data = g_data[id];
+    document.title = data.title;
+    $('h3').html(data.title);
+    $('h5').html(data.user);
+    g_audio.src = 'https://file.koe-koe.com/sound/upload/' + id + '.mp3';
 }
